4203a06bfaa9f1b7d9fa8ece55f60e2c4d7db0b7
mhimranhossain <mh.imranhossain@outlook.com>
2023-04-06 15:18:31 +0600

    Update 04/06/2023 15:18:31

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import { OrbitControls, } from '@react-three/drei'
+import { useRef, useEffect } from 'react'
+import { Html, OrbitControls, PivotControls } from '@react-three/drei'
+import { DoubleSide } from 'three'
 
-export const Cube = () => {
+export const App = () => {
+
   return (
-    <mesh position-x={-2} scale={2} castShadow receiveShadow>
+    <>
+      <ambientLight intensity={0.3} />
+      <spotLight position-y={3} castShadow />
+      <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={2}>
+        <Cube />
+      </PivotControls>
+      <Sphere />
+      <Plane />
+      <OrbitControls makeDefault />
+    </>
+  )
+}
+
+export function Cube() {
+  return (
+    <mesh position-x={-2} scale={1.99} castShadow receiveShadow>
       <boxGeometry />
       <meshStandardMaterial color="orange" />
     </mesh>
   )
 }
-export const Sphere = () => {
+export function Sphere() {
   return (
     <mesh position-x={2} castShadow receiveShadow>
+      <Html className='bg-black/50 p-2 rounded-full whitespace-nowrap' center>👍 That Is A Sphere </Html>
       <sphereGeometry />
       <meshStandardMaterial color="mediumpurple" />
     </mesh>
   )
 }
-export const Plane = () => {
+export function Plane() {
   return (
-    <mesh position-y={- 1} scale={15} rotation-x={-Math.PI * 0.5} receiveShadow>
+    <mesh position-y={-1} scale={15} rotation-x={-Math.PI * 0.5} receiveShadow>
       <planeGeometry />
-      <meshStandardMaterial color="greenyellow" />
+      <meshStandardMaterial color="greenyellow" side={DoubleSide} />
     </mesh>
-  )
-}
-
-export const App = () => {
-  return (
-    <>
-      <ambientLight intensity={0.3} />
-      <spotLight position-y={3} castShadow />
-      <group>
-        <Cube />
-        <Sphere />
-      </group>
-      <Plane />
-      <OrbitControls />
-    </>
   )
 }
